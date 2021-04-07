4e6c41828481e563d5b064a726e0ae1817c290b2
Andrew Martinez <apetermz@gmail.com>
2021-04-07 16:40:53 -0700

    Updates: Pybank & Pypoll. Sill need to move txtfile to analysis folder

---
+++ PyBank/main.py
@@ -0,0 +1,88 @@
+import os
+import csv
+
+#Variable used for storing the months
+date = []
+#Variable used for storing the profit/losses values
+profit_losses = []
+#Variable for storing the difference between the row and next row below
+#There should be 85 values
+monthly_changes = []
+
+#Line for reading a file
+budget_data = os.path.join("Resources/budget_data.csv")
+
+#First two lines to print as stated in instructions
+print("Financial Analysis")
+print("---------------------")
+
+
+# Read in the CSV file
+with open(budget_data, 'r') as csvfile:
+
+	
+    # Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Skip header row when reading
+    header = next(csvreader)
+    #Append month and profit losses into the empty lists of above.
+    for row in csvreader:
+    	date.append(row[0])
+    	profit_losses.append(int(row[1]))
+#Now that we have lists of date variables and profit/loss values, we can manipulate that data.   	
+
+#This loop calculates the changes in "Profit/Losses" over the entire period
+#We are taking into account that there are 85 values in the monthly_changes list
+for i in range(len(profit_losses)-1):
+	previous_row = profit_losses[i]
+	next_row = profit_losses[i+1]
+	#monthly_change = 0
+	profit_loss_avg_change = next_row - previous_row
+	monthly_changes.append(profit_loss_avg_change)
+
+#This function finds the average of the values in the monthly_changes list and rouncs by 2 decimals
+def Average(monthly_changes):
+	return sum(monthly_changes) / len(monthly_changes)
+
+changes = round(Average(monthly_changes),2)
+
+#Variables for greatest increase in profits & greatest decrease in losses
+max_value = max(monthly_changes)
+min_value = min(monthly_changes)
+
+#Print to terminal
+print(f'Total Months: {len(date)}')
+print(f'Total: ${sum(profit_losses)}')
+print(f'Average  Change: ${changes}')
+
+#This loop goes through the date and monthly_changes list and prints the month associated with the greatest increase and decrease values
+for (month_value, avg_value) in zip(date, range(len(monthly_changes)+1)):
+	new_avg_value = monthly_changes[avg_value-1]
+	if(max_value == new_avg_value):
+		print(f'Greatest Increase in Profits: {month_value} ({max_value})')
+	if(min_value == new_avg_value):
+		print(f'Greatest Decrease in Profits: {month_value} ({min_value})')
+
+
+#write to text file
+output_path = os.path.join("textOutput.txt")
+
+with open(output_path, 'w', newline='') as csvfile:
+
+	csvwriter = csv.writer(csvfile, delimiter=',')
+
+	#Similar to printing to the terminal
+	csvwriter.writerow(['Financial Analysis'])
+	csvwriter.writerow(['---------------------'])
+
+	# %s' % found on stack overflow
+	csvwriter.writerow(['Total Months: %s' % len(date)])
+	csvwriter.writerow(['Total: $%s' %  sum(profit_losses)])
+	csvwriter.writerow(['Average Change: $%s' % changes])
+
+	for (month_value, avg_value) in zip(date, range(len(monthly_changes)+1)):
+		new_avg_value = monthly_changes[avg_value-1]
+		if(max_value == new_avg_value):
+			csvwriter.writerow(['Greatest Increase in Profits: {} ({})'.format(month_value,max_value)])
+		if(min_value == new_avg_value):
+			csvwriter.writerow(['Greatest Decrease in Profits: {} ({})'.format(month_value,min_value)])
---
+++ PyPoll/main.py
@@ -0,0 +1,55 @@
+import os
+import csv
+
+pypoll_dict = {}
+
+election_data = os.path.join("Resources/PyPoll_Resources_election_data.csv")
+
+print("Election Results \n------------------------------")
+
+# Read in the CSV file
+with open(election_data, 'r') as csvfile:
+
+	# Split the data on commas
+    csvreader = csv.reader(csvfile, delimiter=',')
+    #Skip header row when reading
+    header = next(csvreader)
+
+
+    total_votes = 0
+
+    for row in csvreader:    #row = [ID, County, Candidate]
+    	total_votes += 1
+    	candidate = row[2]
+    	if candidate not in pypoll_dict:
+    		pypoll_dict[candidate] = 0
+    	pypoll_dict[candidate] += 1
+
+   		 	
+    print("Total Votes: {}\n------------------------------".format(total_votes))
+    for candidate in pypoll_dict:
+    	percentage_vote = format((pypoll_dict[candidate] / total_votes)*100, '.3f')
+    	print("{}: {}% ({})".format(candidate,percentage_vote,pypoll_dict[candidate]))
+    print("------------------------------")
+
+    winner = max(pypoll_dict,key=pypoll_dict.get)
+    print("Winner: {}\n------------------------------".format(winner))
+
+
+#write to text file
+output_path = os.path.join("textOutputPypoll.txt")
+with open(output_path, 'w', newline='') as csvfile:
+	csvwriter = csv.writer(csvfile, delimiter=',')
+
+	csvwriter.writerow(['Election Results']) 
+	csvwriter.writerow(['------------------------------'])
+
+	csvwriter.writerow(['Total Votes: {}'.format(total_votes)])
+	csvwriter.writerow(['------------------------------'])
+	for candidate in pypoll_dict:
+		percentage_vote = format((pypoll_dict[candidate] / total_votes)*100, '.3f')
+		csvwriter.writerow(["{}: {}% ({})".format(candidate,percentage_vote,pypoll_dict[candidate])])
+	csvwriter.writerow(['------------------------------'])
+
+	csvwriter.writerow(['Winner: {}'.format(winner)])
+	csvwriter.writerow(['------------------------------'])
